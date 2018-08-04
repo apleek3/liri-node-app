b56b8caf65f8e04d87f9de8f22c36c66954246e7
apleek3 <apleek3@gmail.com>
2018-08-04 13:22:46 -0700

    npm dotenv and request added)

--- liri.js
+++ liri.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 
 // HOMEWORK INSTRUCTION: Add the code required to import the `keys.js` file and store it in a variable.
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+
+var command = process.argv[2];
+var request = require("request");
+
+
+
+if (command == 'movieThis') {
+  console.log("command");
+  movieThis();
+};
+
 
 function myTweets() {
   //This will show your last 20 tweets and when they were created at in your terminal/bash window.
@@ -10,13 +21,13 @@ function myTweets() {
 
 function spotifyThisSong(song_name) {
   //* This will show the following information about the song in your terminal/bash window
-     
+
   //* Artist(s)
-  
+
   //* The song's name
-  
+
   //* A preview link of the song from Spotify
-  
+
   //* The album that the song is from
 
   //* If no song is provided then your program will default to "The Sign" by Ace of Base.
@@ -25,7 +36,44 @@ function spotifyThisSong(song_name) {
 
 function movieThis(movie_name) {
 
+  // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+  //var request = require("request");
+ console.log("movieThis");
+  // Then run a request to the OMDB API with the movie specified
+  request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function (error, response) {
+
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      console.log("The movie's rating is: " + JSON.parse(response.body).imdbRating);
+    }
+  });
+
+
+
+  /*
+    var splitMovie = command.split(" ");
+    for (i=0; i < splitMovie.length; i++) {
+      var splitMovieString = splitMovie[i].toLowerCase() + "+";
+      }
+    // Then run a request to the OMDB API with the movie specified
+    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function (error, response, body) {
+  
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode === 200) {
+  
+        // Parse the body of the site and recover just the imdbRating
+        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      }
+    });
+  */
+
   //* Use trilogy api key
+
+
   //* Title of the movie.
   //* Year the movie came out.
   //* IMDB Rating of the movie.
@@ -35,15 +83,15 @@ function movieThis(movie_name) {
   //* Plot of the movie.
   //* Actors in the movie.
 
-   //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+  //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 };
 
 function doWhatItSays() {
 
   //* Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-     
+
   //* It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-  
+
   //* Feel free to change the text in that document to test out the feature for other commands.
 };
 
