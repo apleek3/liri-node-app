17c974cfaa60fd1f2df683bf332ec944a926b8a2
apleek3 <apleek3@gmail.com>
2018-08-05 22:03:02 -0700

    added npm-spotify

--- liri.js
+++ liri.js
@@ -1,87 +1,94 @@
 require("dotenv").config();
 
 // HOMEWORK INSTRUCTION: Add the code required to import the `keys.js` file and store it in a variable.
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
 
 var command = process.argv[2];
 var request = require("request");
-
-
+var name = process.argv[3];
 
 if (command == 'movieThis') {
-  console.log("command");
-  movieThis();
+  console.log(command);
+  movieThis(name);
 };
 
+
+/* PULLED FROM NPM SPOTIFY
+var SpotifyWebApi = require('spotify-web-api-node');
+ 
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId: 'fcecfc72172e4cd267473117a17cbd4d',
+  clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+  redirectUri: 'http://www.example.com/callback'
+});
+*/
 
 function myTweets() {
   //This will show your last 20 tweets and when they were created at in your terminal/bash window.
 };
 
-function spotifyThisSong(song_name) {
-  //* This will show the following information about the song in your terminal/bash window
+function spotifyThis(song_name) {
+/*
+  // A request to the OMDB API with the movie specified
+  request("http://www.omdbapi.com/?t=" + movie_name + "&plot=short&apikey=trilogy", function (error, response) {
 
-  //* Artist(s)
+    // Should I need to check the data:
+    // console.log(resonse); 
 
-  //* The song's name
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!error && response.statusCode === 200) {
+      // List of Movie Information per instructions:
+      console.log("This song's Artist is: " + JSON.parse(response.body).Title); //* Artist(s)
+      console.log("This song's Name is: " + JSON.parse(response.body).Year);  //* The song's name
+      console.log("This song's preview can be found at this link: " + JSON.parse(response.body).imdbRating);  //* A preview link of the song from Spotify
+      console.log("This song's Album of origin is: " + JSON.parse(response.body).Ratings[1].Value);  //* The album that the song is from
+    } else {
+      console.log("No song was provided. Here's some information about 'The Sign' by Ace of Base.: " + JSON.parse(response.body).Country);  //* If no song is provided then your program will default to "The Sign" by Ace of Base.
+ */
+    }
+ 
+  });
 
-  //* A preview link of the song from Spotify
 
-  //* The album that the song is from
-
-  //* If no song is provided then your program will default to "The Sign" by Ace of Base.
-};
+}; //END OF SPOTIFYTHIS
 
 
 function movieThis(movie_name) {
 
-  // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-  //var request = require("request");
- console.log("movieThis");
-  // Then run a request to the OMDB API with the movie specified
-  request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function (error, response) {
+  // A request to the OMDB API with the movie specified
+  request("http://www.omdbapi.com/?t=" + movie_name + "&plot=short&apikey=trilogy", function (error, response) {
+
+    // Should I need to check the data:
+    // console.log(resonse); 
 
     // If the request is successful (i.e. if the response status code is 200)
     if (!error && response.statusCode === 200) {
-
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log("The movie's rating is: " + JSON.parse(response.body).imdbRating);
+      // List of Movie Information per instructions:
+      console.log("The movie's Title is: " + JSON.parse(response.body).Title);//* Title of the movie.
+      console.log("The movie's Production Year is: " + JSON.parse(response.body).Year);//* Year the movie came out.
+      console.log("The movie's IMDB Rating is: " + JSON.parse(response.body).imdbRating);//* IMDB Rating of the movie.
+      console.log("The movie's Rotten Tomatoes Score is: " + JSON.parse(response.body).Ratings[1].Value);//* Rotten Tomatoes Rating of the movie.
+      console.log("The movie's Production Country is: " + JSON.parse(response.body).Country);//* Country where the movie was produced.
+      console.log("The movie's Language is: " + JSON.parse(response.body).Language);//* Language of the movie.
+      console.log("The movie's Plot (in summary) is: " + JSON.parse(response.body).Plot);//* Plot of the movie.
+      console.log("The movie's Actor(s) is/are: " + JSON.parse(response.body).Actors);//* Actors in the movie.
     }
   });
 
-
+}; //END OF MOVIETHIS FUNCTION
 
   /*
-    var splitMovie = command.split(" ");
-    for (i=0; i < splitMovie.length; i++) {
-      var splitMovieString = splitMovie[i].toLowerCase() + "+";
-      }
-    // Then run a request to the OMDB API with the movie specified
-    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
   
-      // If the request is successful (i.e. if the response status code is 200)
-      if (!error && response.statusCode === 200) {
   
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-      }
-    });
-  */
-
-  //* Use trilogy api key
-
-
-  //* Title of the movie.
-  //* Year the movie came out.
-  //* IMDB Rating of the movie.
-  //* Rotten Tomatoes Rating of the movie.
-  //* Country where the movie was produced.
-  //* Language of the movie.
-  //* Plot of the movie.
-  //* Actors in the movie.
+  
+  
+  
+  
+  
+  
 
   //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 };
