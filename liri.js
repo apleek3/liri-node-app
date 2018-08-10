77235e6db2cd24bd4e449f9ad157cb1d88268d2e
apleek3 <apleek3@gmail.com>
2018-08-09 18:55:27 -0700

    logger added

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-/************************************************************************************************************** */
+/******************************************LIRI-APP************************************************************ */
 /************************************************************************************************************** */
 /************************************************************************************************************** */
 require("dotenv").config(); //env security connection
@@ -6,31 +6,65 @@ var Spotify = require('node-spotify-api'), //require variables
   request = require("request"),
   express = require('express'),
   Twitter = require("twitter"),
+  fs = require('fs-extra'),
   keys = require("./keys"); // {twitter: consumer_key..., spotify: id: ...}
 
 // HOMEWORK INSTRUCTION: Add the code required to import the `keys.js` file and store it in a variable.
 var spotify = new Spotify(keys.spotify); //new object using Spotify constructor
 var client = new Twitter(keys.twitter); //new object using Twitter constructor
 var command = process.argv[2]; //pulls the commands from the terminal
-// COMMANDS: movieThis, spotifyThis, twitterThis, doThis
-var name = process.argv[3];
-var router = express.Router();
+var name = process.argv[3]; // COMMANDS: movieThis, spotifyThis, twitterThis, doThis
 
+var logger = fs.createWriteStream('./log.txt ', { // used to log commands and data to log.txt
+  flags: 'a' // 'a' means appending (old data will be preserved)
+});
+
+/****************************************DO WHAT IT SAYS******************************************************* */
 /************************************************************************************************************** */
 /************************************************************************************************************** */
-/************************************************************************************************************** */
 
-if (command == 'movieThis') { //calls movieThis command in terminal
-  movieThis(name);
-};
+if (command == 'do-what-it-says') { //calls do-what-it-says command in terminal
+  doWhatItSays = function () {
+    fs.readFile('./random.txt', 'utf8', function (err, data) {
+      if (err) throw err;
+      var random = data.split(", ");
+      name = random[1]; //Stores the second input from random.txt
+      console.log("Your text file's command was '" + random[0] + ".' Your Search Query was '" + random[1] + ".'"); //CONSOLE FEEDBACK
+      logger.write("DO-WHAT-IT-SAYS COMMAND. Reads from random.txt" + "\n");//LOGGER
+      logger.write("COMMAND: " + random[0] + ", ")
+      logger.write("QUERY: " + random[1] + "\n");
+      if (random[0] == 'movieThis') { //calls movieThis command in terminal using input from the random.txt
+        movieThis(name);
+      };
 
-if (command == 'spotifyThis') { //calls spotifyThis command in terminal
-  spotifyThis(name);
-};
+      if (random[0] == 'spotifyThis') { //calls spotifyThis command in terminal using input from the random.txt
+        spotifyThis(name);
+      };
 
-if (command == 'twitterThis') { //calls twitterThis command in terminal
-  myTweets(name);
-};
+      if (random[0] == 'twitterThis') { //calls twitterThis command in terminal using input from the random.txt
+        myTweets(name);
+      };
+    });
+  }
+  doWhatItSays();
+} else {
+
+  // Logs all command and search inputs to log.txt
+  logger.write("COMMAND: " + command + ", ");
+  logger.write("QUERY: " + name + "\n");
+
+  if (command == 'movieThis') { //calls movieThis command in terminal using input from the user
+    movieThis(name);
+  };
+
+  if (command == 'spotifyThis') { //calls spotifyThis command in terminal using input from the user
+    spotifyThis(name);
+  };
+
+  if (command == 'twitterThis') { //calls twitterThis command in terminal using input from the user
+    myTweets(name);
+  };
+}
 
 /*********************************************TWITTER THIS***************************************************** */
 /************************************************************************************************************** */
@@ -41,9 +75,11 @@ function myTweets(user_name) {
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
       console.log(tweets[0].user.name + "'s Last 20 Tweets are:");
-      for (i = 1; i < tweets.length; i++) {
+      for (i = 1; i < tweets.length; i++) { //Loops through the tweets to print out a total of 20 from the user's history
         console.log("Tweet #" + i + " of the last 20  :" + tweets[i].text);
+        logger.write("Tweet #" + i + " of the last 20  :" + tweets[i].text + "\n"); //adds the results to log.txt
       }
+      logger.write("\n");
     }
   });
 };
@@ -66,6 +102,13 @@ function spotifyThis(song_name) {
     console.log("This song is performed by: " + data.tracks.items[0].artists[0].name) //ARTISTS
     console.log("The name of the album this song is from is: " + data.tracks.items[0].album.name); //Album NAME
     console.log("A preview of this song can be found at this link:  " + data.tracks.items[0].preview_url); // PREVIEW url
+
+    //LOGGER
+    logger.write("The song you searched for is: " + data.tracks.items[0].name + "\n"); //Song's NAME
+    logger.write("This song is performed by: " + data.tracks.items[0].artists[0].name + "\n") //ARTISTS
+    logger.write("The name of the album this song is from is: " + data.tracks.items[0].album.name + "\n"); //Album NAME
+    logger.write("A preview of this song can be found at this link:  " + data.tracks.items[0].preview_url + "\n"); // PREVIEW url
+    logger.write("\n");
   });
 };
 
@@ -92,48 +135,18 @@ function movieThis(movie_name) {
       console.log("The movie's Language is: " + JSON.parse(response.body).Language);//* Language of the movie.
       console.log("The movie's Plot (in summary) is: " + JSON.parse(response.body).Plot);//* Plot of the movie.
       console.log("The movie's Actor(s) is/are: " + JSON.parse(response.body).Actors);//* Actors in the movie.
+
+      //LOGGER
+      logger.write("The movie's Title is: " + JSON.parse(response.body).Title + "\n");//* Title of the movie.
+      logger.write("The movie's Production Year is: " + JSON.parse(response.body).Year + "\n");//* Year the movie came out.
+      logger.write("The movie's IMDB Rating is: " + JSON.parse(response.body).imdbRating + "\n");//* IMDB Rating of the movie.
+      logger.write("The movie's Rotten Tomatoes Score is: " + JSON.parse(response.body).Ratings[1].Value + "\n");//* Rotten Tomatoes Rating of the movie.
+      logger.write("The movie's Production Country is: " + JSON.parse(response.body).Country);//* Country where the movie was produced.
+      logger.write("The movie's Language is: " + JSON.parse(response.body).Language + "\n");//* Language of the movie.
+      logger.write("The movie's Plot (in summary) is: " + JSON.parse(response.body).Plot + "\n");//* Plot of the movie.
+      logger.write("The movie's Actor(s) is/are: " + JSON.parse(response.body).Actors + "\n");//* Actors in the movie.
+      logger.write("\n");
     };
   });
 };
 
-
-
-
-
-/***********************************************DO WHAT IT SAYS************************************************ */
-/************************************************************************************************************** */
-/************************************************************************************************************** */
-
-
-function doWhatItSays() {
-
-  //* Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-  //* It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-  //* Feel free to change the text in that document to test out the feature for other commands.
-};
-
-/************************************************************************************************************** */
-/************************************************************************************************************** */
-/************************************************************************************************************** */
-/************************************************************************************************************** */
-/************************************************************************************************************** */
-/************************************************************************************************************** */
-/*
-    * `do-what-it-says`
-
-
-
-4. `node liri.js do-what-it-says`
-   
-   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-     
-     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-     
-     * Feel free to change the text in that document to test out the feature for other commands.
-    * 
-    * 
-  
-  
-    */
